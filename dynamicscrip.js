440621b467328eb0e976d4d27cb73e3078f34f47
Harishmuru <harishmurugesan2304@gmail.com>
2024-06-19 16:59:41 +0530

    Today workout

---
+++ dynamicscrip.js
@@ -0,0 +1,43 @@
+var name1="webpage";
+document.write(name1);
+document.write("<br>");
+let a=4;
+let b=5;
+add=a+b;
+sub=a-b;
+nul=a*b;
+div=a/b;
+document.write(a);
+document.write("<br>");
+str1="ring is the big size";
+start=2;
+end=5;
+document.write(str1.substr(0,5));
+document.write("<br>");
+document.write(str1.slice(start,end));
+document.write("<br>");
+document.write(str1.substring(5));
+document.write("<br>");
+var name2=new Array;
+name2[0]="mathi";
+name2[1]="deva";
+document.write(name2[1]);
+document.write("<br>");
+var animals=["tiger","lion","deer","dog"]
+console.log(animals);
+document.write(animals);
+document.write("<br>");
+document.write(animals.sort());
+document.write("<br>");
+function myarray()
+{
+    var season=['monson','automn','rain','sunny']
+    document.write(season);
+}
+myarray();
+document.write("<br>");
+let myFunction=()=>{
+    var num=45;
+    document.write(num);
+}
+myFunction();
